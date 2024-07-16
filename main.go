ddc6be1ac51a900d2802ee2fa56a74ca5d6dcb78
aniruddha-2846 <aniruddha2846@gmail.com>
2024-07-16 16:41:38 +0530

    shortened func main()

--- main.go
+++ main.go
@@ -2,36 +2,21 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"geektrust/subpackages"
 	"os"
-	"strings"
 )
 
-func getFirstWord(s string) string {
-	words := strings.Fields(s)
-	if len(words) > 0 {
-		return words[0]
-	}
-	return ""
-}
-
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "Please give the input file path")
-		os.Exit(1)
-	}
 	filename := os.Args[1]
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error opening the file")
+	file, err1 := os.Open(filename)
+	if err1 != nil {
 		os.Exit(1)
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		firstWord := getFirstWord(line)
+		firstWord := subpackages.GetFirstWord(line)
 		switch firstWord {
 		case "LOAN":
 			subpackages.ProcessLoan(line)
@@ -40,9 +25,5 @@ func main() {
 		case "BALANCE":
 			subpackages.ProcessBalance(line)
 		}
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading the file")
-		os.Exit(1)
 	}
 }
